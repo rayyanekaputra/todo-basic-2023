0a3aad85c6a66051d99e0b9bcaf0b1ba522b7d9f
rayyanekaputra <bullzhit99@gmail.com>
2023-11-12 06:35:44 +0800

    feat: added a an array of storing inputs

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,21 @@
-'use client'
-import { useState } from 'react';
-import './homeStyle.css'
+"use client";
+import { useState } from "react";
+import "./homeStyle.css";
 
 export default function Home() {
+  const [written, setWritten] = useState("");
+  const [collection, setCollection] = useState([]);
 
-  const [written, setWritten] = useState('')
-  const [note, setNote] = useState('')
-
-  function handlerAddWritten(event){
-    event.preventDefault
-    setWritten(event.target.value)
+  function handlerAddWritten(event) {
+    event.preventDefault();
+    setWritten(event.target.value);
   }
-  function handlerAddNote(){
-    setNote(written)
-    console.log(written)
+  function handlerAddNote() {
+    const UpdateCollection = [...collection, written];
+    setCollection(UpdateCollection);
+    setWritten(""); //reset
+    console.log(written);
   }
-
 
   return (
     <section>
@@ -25,14 +25,29 @@ export default function Home() {
           <p>You can simply make a new task by typing down below</p>
         </div>
         <div className="cta-card-wrapper">
-          <input placeholder="Write something.." onChange={handlerAddWritten}/>
-          <button onClick={handlerAddNote}>Submit</button>
+          <input placeholder="Write something.." onChange={handlerAddWritten} />
+          {written ? (
+            <button onClick={handlerAddNote}>Submit</button>
+          ) : (
+            <button disabled style={{
+              backgroundColor: 'gray'
+            }}>Maybe, type something?</button>
+          )}
         </div>
-        <div className="cta-card-wrapper">
-          <p>{note}</p>
-        </div>
-
       </div>
+      {collection.length > 0 ? (
+        <ul>
+          {collection.map((notes, index) => (
+            <li key={index}>
+              <div className="card-container">
+                <div className="header-card-wrapper">
+                  <p className="truncate">{notes}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </section>
   );
 }
